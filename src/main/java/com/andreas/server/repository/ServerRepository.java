1c4ab3ad0bde04ff9b99ccd12a83532560844c3c
avenet <andre.venett@gmail.com>
2022-01-11 09:48:30 +0200

    initial commit

---
+++ src/main/java/com/andreas/server/repository/ServerRepository.java
@@ -0,0 +1,8 @@
+package com.andreas.server.repository;
+
+import com.andreas.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
---
+++ src/main/java/com/andreas/server/resource/ServerResource.java
@@ -0,0 +1,34 @@
+package com.andreas.server.resource;
+
+import com.andreas.server.model.Response;
+import com.andreas.server.service.implementation.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timestamp(now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
